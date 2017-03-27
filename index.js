feed5080952c5fee7f23666d78725c209ca6f4ed
wolyshaw <shaw@xwlong.com>
2017-03-27 11:26:29 +0800

    fix image load error

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const proxy = config => {
 		let opt, _req
 
 		opt = {
-				hostname: config.hostname,
+				host: config.hostname,
 				path: req.url,
 				method: req.method
 			}
@@ -13,14 +13,9 @@ const proxy = config => {
 		_req = http.request(opt, _res => {
 			if (_res.statusCode === 200) {
 				console.log(`remote: http status code is  ${_res.statusCode} url is ${req.url}`)
-				_res.setEncoding('utf-8')
-				let content = []
-				_res
-					.on('data', chunk => {
-						content.push(chunk)
-					})
-					.on('end', () => res.status(_res.statusCode).send(content.join('')))
+				_res.pipe(res, 'utf-8')
 			}else{
+				console.log(`local: http status code is  ${_res.statusCode} url is ${req.url}`)
 				next()
 			}
 		})
